db97d8624365d72590aa2f4ddd01bca369078935
zgbsss <gabrielasaraiva.04@gmail.com>
2023-06-02 16:59:54 -0300

    Criação das functons e router da Editora

---
+++ src/api/editoras.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+export default class EditorasAPI {
+  async buscarTodasAsEditoras() {
+    const { data } = await axios.get('/editoras/');
+    return data;
+  }
+  async adicionarEditora(editora) {
+    const { data } = await axios.post('/editoras/', editora);
+    return data;
+  }
+  async atualizarEditora(editora) {
+    const { data } = await axios.put(`/editoras/${editora.id}/`, editora);
+    return data;
+  }
+  async deletarEditora(id) {
+    const { data } = await axios.delete(`/categorias/${id}/`);
+    return data;
+  }
+}
